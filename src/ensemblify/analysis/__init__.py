fe72ea3aa80586364ec9f6004460ebcaae276052
NunoF771 <nuno.pires.fernandes@hotmail.com>
2024-10-08 16:22:13 +0100

    swapped distance and contact matrix order in init

--- src/ensemblify/analysis/__init__.py
+++ src/ensemblify/analysis/__init__.py
@@ -13,21 +13,22 @@ Typical usage example:
 
 from ensemblify.analysis.trajectory import analyze_trajectory
 from ensemblify.analysis.trajectory_utils import (calculate_analysis_data,create_analysis_figures,
-                                                  calculate_ramachandran_data,calculate_distance_matrix,
-                                                  calculate_contact_matrix,calculate_ss_assignment,
+                                                  calculate_ramachandran_data,calculate_contact_matrix,
+                                                  calculate_distance_matrix,calculate_ss_assignment,
                                                   calculate_ss_frequency,calculate_metrics_data,
-                                                  create_contact_map_fig,create_ss_frequency_figure,
-                                                  create_metrics_fig)
+                                                  create_contact_map_fig,create_distance_matrix_fig,
+                                                  create_ss_frequency_figure,create_metrics_fig)
 
 __all__ = ['analyze_trajectory',
            'calculate_analysis_data',
            'create_analysis_figures',
            'calculate_ramachandran_data',
-           'calculate_distance_matrix',
            'calculate_contact_matrix',
+           'calculate_distance_matrix',
            'calculate_ss_assignment',
            'calculate_ss_frequency',
            'calculate_metrics_data',
            'create_contact_map_fig',
+           'create_distance_matrix_fig',
            'create_ss_frequency_figure',
            'create_metrics_fig']
